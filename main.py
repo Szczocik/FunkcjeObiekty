8a58ed816bc259080a0ecf2a667a61313873000c
popam <popa.mariusz@gmail.com>
2021-08-30 17:50:46 +0200

    skladnia

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #  plik warsztatowy Funkcje Obiekty PEP
 
+# Funkcje to nazwane i odizolowane fragmenty kodu, które opcjonalnie mogą zwracać
+# wartość: funkcje definiuje się przez jej nazwę, parametry i ciało (kod właśiciwy funkcji).
+
+# Składnia:
+def nazwa_funkcji(parametr1, parametr2, parametr3, ... ,parametrn): #dwukropek na końcu
+	kod do wykonania linia 1 #zwróć uwagę na wcięcie
+	kod do wykonania linia 1
+ 	kod do wykonania linia 1
+
