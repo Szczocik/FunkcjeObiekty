0774f1b60f2b5c9619f90d61eabe381a989b8a2c
popam <popa.mariusz@gmail.com>
2021-08-31 12:40:42 +0200

    definicje argumenty pozycyjne i przypisane

--- main.py
+++ main.py
@@ -4,45 +4,60 @@
 # wartość: funkcje definiuje się przez jej nazwę, parametry i ciało (kod właśiciwy funkcji).
 
 # Składnia:
+# def nazwa_funkcji(parametr1, parametr2, parametr3, ... ,parametrn): #dwukropek na końcu
+# 	kod do wykonania linia 1 #zwróć uwagę na wcięcie
+# 	kod do wykonania linia 1
+#  	kod do wykonania linia 1
+
+
+# Funkcje
 '''
-def nazwa_funkcji(parametr1, parametr2, parametr3, ... ,parametrn): #dwukropek na końcu
-	kod do wykonania linia 1 #zwróć uwagę na wcięcie
-	kod do wykonania linia 1
- 	kod do wykonania linia 1
+def show_separator(number):
+    print("#"*number)
 
-rows = [
-  ("Styczeń", {"Saldo": 1200, "Wpłaty": 1800, "Wydatki": 1300}),
-  ("Luty", {"Saldo": 1300, "Wpłaty": 1900, "Wydatki": 1200}),
-]
+show_separator(50)
+number_1 = float(input('Liczba 1: '))
+number_2 = float(input('Liczba 2: '))
+show_separator(20)
+number_sum = number_1 + number_2
+print(f'Suma liczb {number_1} i {number_2}')
+show_separator(10)
 
-for name, row in rows:
-  print("*" * 10)
-  print("Saldo miesięczne: {}".format(name))
-  print("*" * 10)
-  for k,v in row.items():
-  	print("{}: {}".format(k, v))
-print("*" * 10)
+def sum_numbers(number1, number2):
+    result = number1 + number2
+    return result
+result = sum_numbers(2, 3)
+print("Result:", result)
+
+def sum_numbers(number1, number2):
+    result = number1 + number2
+    diff = number1 - number2
+    return (result, diff)  # przy 2 działaniach możemy otrzymać Tuplę
+result_sum, result_diff = sum_numbers(2, 3)  # możemy odrazu przyporządkować Tuplę
+print("Result:", result_sum, result_diff)
+
+def sort_numbers(a, b, c):  # w nawiasie argumenty pozycyjne
+    temp = [a, b, c]
+    return temp
+
+sorted_numbers = sort_numbers(2, 3, 1)
+print(sorted_numbers)
+def sort_numbers(a, b, c):  # w nawiasie argumenty pozycyjne
+    temp = [a, b, c]
+    return sorted_temp
+
+sorted_numbers = sort_numbers(2, 3, 1)
+print(sorted_numbers)
+
+
+sorted_numbers = sort_numbers(2, 3, c=1, d=3)  # argumenty pozycyjne muszą być przed przypisanymi (c=1)
+print(sorted_numbers)
 '''
+def sort_numbers(a, b, c=1, d=3):  # w nawiasie argumenty pozycyjne
+    print("c", c)
+    print("d", d)
+    temp = [a, b, c, d]  # lista
+    return sorted(temp)  # zwraca sortowanie listy
 
-owner = {"Imię": "Adam", "Nazwisko": "Kowalski"}
-rows = [
-  ("Styczeń", {"Saldo": 1200, "Wpłaty": 1800, "Wydatki": 1300}),
-  ("Luty", {"Saldo": 1300, "Wpłaty": 1900, "Wydatki": 1200}),
-]
-
-
-print("*" * 10)
-print("Właściciel konta")
-print("*" * 10)
-for k,v in owner.items():
-  print("{}: {}".format(k, v)
-print("*" * 10)
-
-for name, row in rows:
-  print("*" * 10)
-   print("Saldo miesięczne: {}".format(name))
-  print("*" * 10)
-  for k,v in row.items():
-  	print("{}: {}".format(k, v))
-  print("*" * 10)
-
+sorted_numbers = sort_numbers(2, 3, d=1, c=5)  # nie ma znaczenia kolejność argumentów przypisanych (c,d)
+print(sorted_numbers)
